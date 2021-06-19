074268c1797a0209342ae96d23d6301ee8a14181
BlG-SEM <dolgihsemen@mail.ru>
2021-06-19 12:22:56 +0300

    Task7

---
+++ src/ru/vsu/cs/Main2.java
@@ -0,0 +1,16 @@
+package ru.vsu.cs;
+
+import ru.vsu.cs.graph.MyGraph;
+
+public class Main2 {
+
+    // Демонстрация записи десериализации файла в граф и сериализации графа в файл
+    public static void main(String[] args) {
+        String pathToGraphInputFile = "res/graph.txt";
+        String pathToGraphOutputFile = "res/savedGraph.txt";
+
+        MyGraph<Integer> graph = GraphUtils.readGraphFromFile(pathToGraphInputFile);
+        graph.print();
+        GraphUtils.writeToFile(graph, pathToGraphOutputFile);
+    }
+}
